a8f356ab42298f5393936cd69839861c735d8423
Luke Hill <lukehill_uk@hotmail.com>
2023-10-05 17:23:24 +0100

    Fix rename of found -> detected

--- ruby/lib/cck/messages_comparator.rb
+++ ruby/lib/cck/messages_comparator.rb
@@ -4,12 +4,12 @@ require_relative 'keys_checker'
 
 module CCK
   class MessagesComparator
-    def initialize(validator, found, expected)
+    def initialize(validator, detected, expected)
       @all_errors = []
       @compared = []
       @validator = validator
 
-      compare(found, expected)
+      compare(detected, expected)
     end
 
     def errors
@@ -17,20 +17,17 @@ module CCK
     end
 
     def debug
-      # puts 'Compared the following type of message:'
-      # puts @compared.uniq.map { |m| " - #{m}" }.join("\n")
-      # puts ''
       puts errors.uniq.join("\n")
     end
 
     private
 
-    def compare(found, expected)
-      found_by_type = messages_by_type(found)
+    def compare(detected, expected)
+      detected_by_type = messages_by_type(detected)
       expected_by_type = messages_by_type(expected)
 
-      found_by_type.keys.each do |type|
-        compare_list(found_by_type[type], expected_by_type[type])
+      detected_by_type.keys.each do |type|
+        compare_list(detected_by_type[type], expected_by_type[type])
       rescue StandardError => e
         @all_errors << "Error whild comparing #{type}: #{e.message}"
       end
@@ -54,33 +51,33 @@ module CCK
       message.send(message_type(message))
     end
 
-    def compare_list(found, expected)
-      found.each_with_index do |message, index|
+    def compare_list(detected, expected)
+      detected.each_with_index do |message, index|
         compare_message(message, expected[index])
       end
     end
 
-    def compare_message(found, expected)
-      return unless found.is_a?(Cucumber::Messages::Message)
-      return if found.is_a?(Cucumber::Messages::GherkinDocument)
-      return if found.is_a?(Cucumber::Messages::Pickle)
-      return if found.is_a?(Cucumber::Messages::Timestamp) && expected.is_a?(Cucumber::Messages::Timestamp)
-      return if found.is_a?(Cucumber::Messages::Duration) && expected.is_a?(Cucumber::Messages::Duration)
-      return if ENV['CI'] && found.is_a?(Cucumber::Messages::Ci) && expected.nil?
+    def compare_message(detected, expected)
+      return unless detected.is_a?(Cucumber::Messages::Message)
+      return if detected.is_a?(Cucumber::Messages::GherkinDocument)
+      return if detected.is_a?(Cucumber::Messages::Pickle)
+      return if detected.is_a?(Cucumber::Messages::Timestamp) && expected.is_a?(Cucumber::Messages::Timestamp)
+      return if detected.is_a?(Cucumber::Messages::Duration) && expected.is_a?(Cucumber::Messages::Duration)
+      return if ENV['CI'] && detected.is_a?(Cucumber::Messages::Ci) && expected.nil?
 
-      @compared << found.class.name
-      @all_errors << @validator.compare(found, expected)
-      compare_sub_messages(found, expected)
+      @compared << detected.class.name
+      @all_errors << @validator.compare(detected, expected)
+      compare_sub_messages(detected, expected)
     end
 
-    def compare_sub_messages(found, expected)
+    def compare_sub_messages(detected, expected)
       return unless expected.respond_to? :to_h
       expected.to_h.keys.each do |key|
         value = expected.send(key)
         if value.is_a?(Array)
-          compare_list(found.send(key), value)
+          compare_list(detected.send(key), value)
         else
-          compare_message(found.send(key), value)
+          compare_message(detected.send(key), value)
         end
       end
     end
