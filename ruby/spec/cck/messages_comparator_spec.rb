53ff8bad7f9b6cda57e47d4fee48e2eed3acb0d7
Luke Hill <lukehill_uk@hotmail.com>
2023-10-05 17:53:41 +0100

    DRY down the message_comparator de-duplication meta spec

--- ruby/spec/cck/messages_comparator_spec.rb
+++ ruby/spec/cck/messages_comparator_spec.rb
@@ -8,28 +8,19 @@ describe CCK::MessagesComparator do
   context 'when executed as part of a CI' do
     before { allow(ENV).to receive(:[]).with('CI').and_return(true) }
 
+    let(:ci_message) { Cucumber::Messages::Ci.new(name: 'Some CI') }
+    let(:meta_message) { Cucumber::Messages::Meta.new }
+    let(:ci_message_envelope) { Cucumber::Messages::Envelope.new(meta: ci_message) }
+    let(:meta_message_envelope) { Cucumber::Messages::Envelope.new(meta: meta_message) }
+
     it 'ignores any detected CI messages' do
-      detected_message_ci = Cucumber::Messages::Ci.new(name: 'Some CI')
-      detected_message_meta = Cucumber::Messages::Meta.new(ci: detected_message_ci)
-      detected_message_envelope = Cucumber::Messages::Envelope.new(meta: detected_message_meta)
-
-      expected_message_meta = Cucumber::Messages::Meta.new
-      expected_message_envelope = Cucumber::Messages::Envelope.new(meta: expected_message_meta)
-
-      comparator = described_class.new(CCK::KeysChecker, [detected_message_envelope], [expected_message_envelope])
+      comparator = described_class.new(CCK::KeysChecker, [ci_message_envelope], [meta_message_envelope])
 
       expect(comparator.errors).to be_empty
     end
 
     it 'ignores any expected CI messages' do
-      detected_message_meta = Cucumber::Messages::Meta.new
-      detected_message_envelope = Cucumber::Messages::Envelope.new(meta: detected_message_meta)
-
-      expected_message_ci = Cucumber::Messages::Ci.new(name: 'Some CI')
-      expected_message_meta = Cucumber::Messages::Meta.new(ci: expected_message_ci)
-      expected_message_envelope = Cucumber::Messages::Envelope.new(meta: expected_message_meta)
-
-      comparator = described_class.new(CCK::KeysChecker, [detected_message_envelope], [expected_message_envelope])
+      comparator = described_class.new(CCK::KeysChecker, [meta_message_envelope], [ci_message_envelope])
 
       expect(comparator.errors).to be_empty
     end
