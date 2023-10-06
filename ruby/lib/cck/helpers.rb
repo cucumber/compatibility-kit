96840304f0c7e1e4ae2364e249c6430abdf090ce
Luke Hill <lukehill_uk@hotmail.com>
2023-10-06 15:44:21 +0100

    Set Helpers to an actual module

--- ruby/lib/cck/helpers.rb
+++ ruby/lib/cck/helpers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module CCK
-  class Helpers
+  module Helpers
     def message_type(message)
       message.to_h.each do |key, value|
         return key unless value.nil?
