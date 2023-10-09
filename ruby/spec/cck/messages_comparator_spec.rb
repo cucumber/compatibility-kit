a6aea65635fdc559135be1da3e664f7a793280a4
Luke Hill <lukehill_uk@hotmail.com>
2023-10-09 18:01:25 +0100

    Revert tests back to old form, slightly DRY up

--- ruby/spec/cck/messages_comparator_spec.rb
+++ ruby/spec/cck/messages_comparator_spec.rb
@@ -9,18 +9,22 @@ describe CCK::MessagesComparator do
     before { allow(ENV).to receive(:[]).with('CI').and_return(true) }
 
     let(:ci_message) { Cucumber::Messages::Ci.new(name: 'Some CI') }
-    let(:meta_message) { Cucumber::Messages::Meta.new }
+    let(:blank_meta_message) { Cucumber::Messages::Meta.new }
     let(:ci_message_envelope) { Cucumber::Messages::Envelope.new(meta: ci_message) }
-    let(:meta_message_envelope) { Cucumber::Messages::Envelope.new(meta: meta_message) }
+    let(:meta_message_envelope) { Cucumber::Messages::Envelope.new(meta: blank_meta_message) }
 
     it 'ignores any detected CI messages' do
-      comparator = described_class.new(CCK::KeysChecker, [ci_message_envelope], [meta_message_envelope])
+      detected_message_meta = Cucumber::Messages::Meta.new(ci: ci_message)
+      detected_message_envelope = Cucumber::Messages::Envelope.new(meta: detected_message_meta)
+      comparator = described_class.new(CCK::KeysChecker, [detected_message_envelope], [meta_message_envelope])
 
       expect(comparator.errors).to be_empty
     end
 
     it 'ignores any expected CI messages' do
-      comparator = described_class.new(CCK::KeysChecker, [meta_message_envelope], [ci_message_envelope])
+      expected_message_meta = Cucumber::Messages::Meta.new(ci: ci_message)
+      expected_message_envelope = Cucumber::Messages::Envelope.new(meta: expected_message_meta)
+      comparator = described_class.new(CCK::KeysChecker, [meta_message_envelope], [expected_message_envelope])
 
       expect(comparator.errors).to be_empty
     end
