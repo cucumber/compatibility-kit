5d21efe494f724222d4e53cf46d97e3cf839b729
Luke Hill <lukehill_uk@hotmail.com>
2023-10-10 15:04:16 +0100

    Raise runtime error

--- ruby/features/unknown-parameter-type/unknown-parameter-type.feature.rb
+++ ruby/features/unknown-parameter-type/unknown-parameter-type.feature.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Given('{airport} is closed because of a strike') do |_airport|
-  raise StandardError, 'Should not be called because airport parameter type has not been defined'
+  raise 'Should not be called because airport parameter type has not been defined'
 end
