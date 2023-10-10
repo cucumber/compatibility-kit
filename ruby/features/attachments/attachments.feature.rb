22fbda170565c01d3eac5ec4fb3383a0024cb222
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2023-10-10 15:57:35 +0100

    [Ruby]  remove legacy ruby code (#63)
    * Remove attach or embed method from hooks, use default filename
    
    * Remove attach or embed method from attachments, use dynamic filename in final step
    
    * Re-order and standardise steps
    
    * Update readme
    
    * Add changelog

--- ruby/features/attachments/attachments.feature.rb
+++ ruby/features/attachments/attachments.feature.rb
@@ -3,55 +3,44 @@
 require 'stringio'
 
 # Cucumber-JVM needs to use a Before hook in order to create attachments
-Before do
-  # no-op
-end
-
-def attach_or_embed(world, data, media_type, filename = nil)
-  # Backward compatibility as the steps are also used by cucumber-ruby 3 which does not support `attach`
-  world.respond_to?(:attach) ? attach(data, media_type, filename) : embed(data, media_type)
-end
+Before { nil }
 
 When('the string {string} is attached as {string}') do |text, media_type|
-  attach_or_embed(self, text, media_type)
+  attach(text, media_type)
 end
 
 When('the string {string} is logged') do |text|
-  # Backward compatibility
-  self.respond_to?(:log) ? log(text) : puts(text)
-end
-
-When('the following string is attached as {string}:') do |media_type, doc_string|
-  attach_or_embed(self, doc_string, media_type)
+  log(text)
 end
 
 When('text with ANSI escapes is logged') do
-  text = "This displays a \x1b[31mr\x1b[0m\x1b[91ma\x1b[0m\x1b[33mi\x1b[0m\x1b[32mn\x1b[0m\x1b[34mb\x1b[0m\x1b[95mo\x1b[0m\x1b[35mw\x1b[0m"
+  log("This displays a \x1b[31mr\x1b[0m\x1b[91ma\x1b[0m\x1b[33mi\x1b[0m\x1b[32mn\x1b[0m\x1b[34mb\x1b[0m\x1b[95mo\x1b[0m\x1b[35mw\x1b[0m")
+end
 
-  self.respond_to?(:log) ? log(text) : puts(text)
+When('the following string is attached as {string}:') do |media_type, doc_string|
+  attach(doc_string, media_type)
 end
 
 When('an array with {int} bytes is attached as {string}') do |size, media_type|
   data = (0..size-1).map { |i| [i].pack('C') }.join
-  attach_or_embed(self, data, media_type)
+  attach(data, media_type)
 end
 
 When('a stream with {int} bytes are attached as {string}') do |size, media_type|
   stream = StringIO.new
   stream.puts (0..size).map(&:to_s).join('')
   stream.seek(0)
-
-  attach_or_embed(self, stream, media_type)
+  attach(stream, media_type)
 end
 
 When('a JPEG image is attached') do
-  attach_or_embed(self, File.open("#{__dir__}/cucumber.jpeg"), 'image/jpeg')
+  attach(File.open("#{__dir__}/cucumber.jpeg"), 'image/jpeg')
 end
 
 When('the {word} png is attached') do |filename|
-  attach_or_embed(self, File.open("#{__dir__}/#{filename}"), 'image/png')
+  attach(File.open("#{__dir__}/#{filename}"), 'image/png')
 end
 
 When('a PDF document is attached and renamed') do
-  attach_or_embed(self, File.open("#{__dir__}/document.pdf"), 'document/pdf', 'renamed.pdf')
+  attach(File.open("#{__dir__}/document.pdf"), 'document/pdf', 'renamed.pdf')
 end
--- ruby/features/hooks/hooks.feature.rb
+++ ruby/features/hooks/hooks.feature.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-def attach_or_embed(world, data, media_type)
-  # Backward compatibility as the steps are also used by cucumber-ruby 3 which does not support `attach`
-  world.respond_to?(:attach) ? attach(data, media_type) : embed(data, media_type)
-end
-
 Before do
   # no-op
 end
@@ -30,5 +25,5 @@ After('@some-tag or @some-other-tag') do
 end
 
 After('@with-attachment') do
-  attach_or_embed(self, File.open("#{__dir__}/cucumber.svg"), 'image/svg+xml')
+  attach(File.open("#{__dir__}/cucumber.svg"), 'image/svg+xml')
 end
