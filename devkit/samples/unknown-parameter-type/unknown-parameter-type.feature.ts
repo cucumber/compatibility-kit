b3ae0766bf9a0ef6acb4ad3459e2ae61363391c3
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2023-10-11 21:01:40 +0100

    CCK: Reconcile unknown-parameter-type (#61)
    * Standardise error message and all unknowns for unknown param type
    
    * Raise runtime error

--- devkit/samples/unknown-parameter-type/unknown-parameter-type.feature.ts
+++ devkit/samples/unknown-parameter-type/unknown-parameter-type.feature.ts
@@ -2,5 +2,5 @@ import { Given } from '@cucumber/fake-cucumber'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 Given('{airport} is closed because of a strike', function (airport) {
-  throw new Error('Should not be called because airport type not defined')
+  throw new Error('Should not be called because airport parameter type has not been defined')
 })
