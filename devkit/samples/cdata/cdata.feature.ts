faf61ea108244d146fc7f355395badee1f7caf97
Luke Hill <lukehill_uk@hotmail.com>
2023-10-11 21:18:43 +0100

    Reconcile step defs for cdata test

--- devkit/samples/cdata/cdata.feature.ts
+++ devkit/samples/cdata/cdata.feature.ts
@@ -1,6 +1,5 @@
-import assert from 'assert'
 import { Given } from '@cucumber/fake-cucumber'
 
 Given('I have {int} <![CDATA[cukes]]> in my belly', function (cukeCount: number) {
-  assert(cukeCount)
+  // no-op
 })
