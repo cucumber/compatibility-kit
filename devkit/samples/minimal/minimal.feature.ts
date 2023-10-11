b53573c06e247932bfd0b7fa73ceaa489c3ba518
Luke Hill <lukehill_uk@hotmail.com>
2023-10-11 22:53:21 +0100

    Reconcile minimal

--- devkit/samples/minimal/minimal.feature.ts
+++ devkit/samples/minimal/minimal.feature.ts
@@ -1,6 +1,5 @@
-import assert from 'assert'
 import { Given } from '@cucumber/fake-cucumber'
 
 Given('I have {int} cukes in my belly', function (cukeCount: number) {
-  assert(cukeCount)
+  // no-op
 })
