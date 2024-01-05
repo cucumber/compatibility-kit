6700cfded1bcbb75287a52a1f2d697dc443115ea
Luke Hill <lukehill_uk@hotmail.com>
2024-01-05 10:33:17 +0000

    Remove all step def code

--- ruby/features/data-tables/data-tables.feature.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-When('the following table is transposed:') do |table|
-  @transposed = table.transpose
-end
-
-Then('it should be:') do |expected|
-  @transposed.diff!(expected)
-end
--- ruby/features/unknown-parameter-type/unknown-parameter-type.feature.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Given('{airport} is closed because of a strike') do |_airport|
-  raise 'Should not be called because airport parameter type has not been defined'
-end
