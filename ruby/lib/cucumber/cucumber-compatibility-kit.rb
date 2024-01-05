53e760f6f5a0513db3479e02b074f0bc4cbd8274
Luke Hill <lukehill_uk@hotmail.com>
2024-01-05 10:07:29 +0000

    Convert to cucumber ruby names / structure for the example detector

--- ruby/lib/cucumber/cucumber-compatibility-kit.rb
+++ ruby/lib/cucumber/cucumber-compatibility-kit.rb
@@ -1,43 +1,41 @@
 # frozen_string_literal: true
 
-require 'shared_examples'
-
-module Cucumber
-  module CompatibilityKit
+module CCK
+  module Examples
     class << self
-      def all_examples
-        gherkin_examples + markdown_examples
+      def all
+        gherkin + markdown
       end
 
-      def gherkin_examples
-        Dir.entries(examples_path).select do |file_or_folder|
+      def gherkin
+        Dir.entries(features_folder_location).select do |file_or_folder|
           next if file_or_folder.start_with?('.')
 
-          gherkin_example?(File.join(examples_path, file_or_folder))
+          gherkin_example?(File.join(features_folder_location, file_or_folder))
         end
       end
 
-      def markdown_examples
-        Dir.entries(examples_path).select do |file_or_folder|
+      def markdown
+        Dir.entries(features_folder_location).select do |file_or_folder|
           next if file_or_folder.start_with?('.')
 
-          markdown_example?(File.join(examples_path, file_or_folder))
+          markdown_example?(File.join(features_folder_location, file_or_folder))
         end
       end
 
-      def examples_path
-        File.expand_path("#{File.dirname(__FILE__)}/../../features/")
-      end
-
-      def example_path(example_name)
-        path = File.join(examples_path, example_name)
+      def supporting_code_for(example_name)
+        path = File.join(features_folder_location, example_name)
 
         return path if File.directory?(path)
 
-        raise ArgumentError, "No CCK example exists for #{example_name}"
+        raise ArgumentError, "No supporting code directory found locally for CCK example: #{example_name}"
       end
 
       private
+
+      def features_folder_location
+        File.expand_path("#{File.dirname(__FILE__)}/../../features/")
+      end
 
       def gherkin_example?(example_folder)
         file_type_in_folder?('.feature', example_folder)
