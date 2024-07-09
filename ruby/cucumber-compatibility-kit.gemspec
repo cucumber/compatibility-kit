9afd4f050e6ce96c99a36f97abf08626dbc1110c
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2024-07-09 13:59:47 +0100

    Refactor/prep for v16 (#105)
    * Update changelog
    
    * Update ruby version to 3.0 minimum
    
    * Update to latest version of rubocop
    
    * Update all other rubocop dependencies
    
    * Update GHA for new ruby versions
    
    * Remove the shim file that exists to move you to the helper methods
    
    Document the helper methods
    
    * Update changelog
    
    * Fix missing change to load path

--- ruby/cucumber-compatibility-kit.gemspec
+++ ruby/cucumber-compatibility-kit.gemspec
@@ -10,8 +10,8 @@ Gem::Specification.new do |s|
   s.homepage    = 'https://github.com/cucumber/compatibility-kit'
   s.platform    = Gem::Platform::RUBY
   s.license     = 'MIT'
-  s.required_ruby_version = '>= 2.6'
-  s.required_rubygems_version = '>= 3.0.9'
+  s.required_ruby_version = '>= 3.0'
+  s.required_rubygems_version = '>= 3.2.8'
 
   s.metadata = {
     'bug_tracker_uri' => 'https://github.com/cucumber/compatibility-kit/issues',
@@ -24,9 +24,9 @@ Gem::Specification.new do |s|
   s.add_dependency 'cucumber-messages', '> 20', '< 26'
 
   s.add_development_dependency 'rspec', '~> 3.13'
-  s.add_development_dependency 'rubocop', '~> 1.50.2'
-  s.add_development_dependency 'rubocop-performance', '~> 1.17.1'
-  s.add_development_dependency 'rubocop-rspec', '~> 2.20.0'
+  s.add_development_dependency 'rubocop', '~> 1.64.1'
+  s.add_development_dependency 'rubocop-performance', '~> 1.21.1'
+  s.add_development_dependency 'rubocop-rspec', '~> 3.0.2'
 
   s.files            = Dir['README.md', 'LICENSE', 'lib/**/*', 'features/**/*']
   s.rdoc_options     = ['--charset=UTF-8']
--- ruby/lib/cck/examples.rb
+++ ruby/lib/cck/examples.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module CCK
+  # CCK::Examples
+  # The helper methods to determine the paths for each relevant item
   module Examples
     class << self
+      # Return the paths for each example that is of 'gherkin' type
       def gherkin
         Dir.entries(cck_features_folder_location).select do |file_or_folder|
           next if file_or_folder.start_with?('.')
@@ -11,6 +14,8 @@ module CCK
         end
       end
 
+      # Return the paths for each example that is of 'markdown' type
+      # NB: At the moment this (1 example - 'markdown'), isn't compatible with cucumber-ruby
       def markdown
         Dir.entries(cck_features_folder_location).select do |file_or_folder|
           next if file_or_folder.start_with?('.')
@@ -19,6 +24,10 @@ module CCK
         end
       end
 
+      # Return the path for a specific example scenario based on its name
+      #
+      # i.e. 'attachments' would return the fully qualified path to the attachments folder
+      # which contains the feature, the ndjson file and any applicable assets to run the example
       def feature_code_for(example_name)
         path = File.join(cck_features_folder_location, example_name)
 
