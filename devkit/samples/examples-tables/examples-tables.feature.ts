37d60debc9a2079399fd9c86d90f34f9e7680cb3
Luke Hill <lukehill_uk@hotmail.com>
2024-07-09 12:45:21 +0100

    Minor tweaks to tidy up cck

--- devkit/samples/examples-tables/examples-tables.feature.ts
+++ devkit/samples/examples-tables/examples-tables.feature.ts
@@ -5,16 +5,16 @@ Given('there are {int} cucumbers', function (initialCount) {
   this.count = initialCount
 })
 
+Given('there are {int} friends', function (initialFriends) {
+  this.friends = initialFriends
+})
+
 When('I eat {int} cucumbers', function (eatCount) {
   this.count -= eatCount
 })
 
 Then('I should have {int} cucumbers', function (expectedCount) {
   assert.strictEqual(this.count, expectedCount)
-})
-
-Given('there are {int} friends', function (initialFriends) {
-  this.friends = initialFriends
 })
 
 Then('each person can eat {int} cucumbers', function (expectedShare) {
