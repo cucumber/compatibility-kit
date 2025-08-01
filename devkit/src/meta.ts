ab432a0377aff514d47940c27442a06e7793e75d
David Goss <david@davidgoss.co>
2025-08-01 09:00:12 +0100

    stable meta

--- devkit/src/meta.ts
+++ devkit/src/meta.ts
@@ -1,34 +1,22 @@
-import fs from 'node:fs'
-import os from 'node:os'
-import path from 'node:path'
-
 import detectCiEnvironment from '@cucumber/ci-environment'
 import { Meta, version as protocolVersion } from '@cucumber/messages'
-
-const { version } = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '..', 'package.json'), {
-    encoding: 'utf-8',
-  })
-) as {
-  version: string
-}
 
 export const meta: Meta = {
   protocolVersion,
   implementation: {
     name: 'fake-cucumber',
-    version,
+    version: '123.45.6',
   },
   cpu: {
-    name: os.arch(),
+    name: 'arm64',
   },
   os: {
-    name: os.platform(),
-    version: os.release(),
+    name: 'darwin',
+    version: '24.5.0',
   },
   runtime: {
     name: 'Node.js',
-    version: process.versions.node,
+    version: '24.4.1',
   },
   ci: detectCiEnvironment(process.env),
 }
