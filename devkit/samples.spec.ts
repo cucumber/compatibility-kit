7f5cd3026ad0129cbd24036f9524fa8329e7375f
David Goss <david@davidgoss.co>
2025-08-02 16:46:35 +0100

    move validation into vitest

--- devkit/samples.spec.ts
+++ devkit/samples.spec.ts
@@ -1,62 +1,115 @@
 import path from 'node:path'
-import fs from 'node:fs'
+import fs, {createReadStream} from 'node:fs'
 import {execFile} from 'node:child_process'
 
 import {describe, it} from 'vitest'
 import globby from "globby";
+import Ajv from "ajv/dist/2020";
+import {Envelope} from "@cucumber/messages";
+import {pipeline} from "node:stream/promises";
+import {NdjsonToMessageStream} from "@cucumber/message-streams";
+import {Writable} from "node:stream";
 
 describe('Samples', async () => {
-  const directories = await globby(['samples/*/'], { onlyDirectories: true })
-  for (const directory of directories) {
-    const suite = path.basename(directory)
+    const directories = await globby(['samples/*/'], {onlyDirectories: true})
 
-    it.concurrent(suite, async function ({expect}) {
-        const featurePaths = (await globby(['*.feature', '*.feature.md'], {cwd: directory})).map(filename => path.join(directory, filename))
-        const args = [...featurePaths]
-        const argumentsPath = path.join(
-            process.cwd(),
-            'samples',
-            suite,
-            suite + '.arguments.txt'
-        )
-      if (fs.existsSync(argumentsPath)) {
-        args.push(...fs.readFileSync(argumentsPath, {encoding: 'utf-8'})
-            .trim()
-            .split(' '))
-      }
+    describe('Generation', async () => {
+        for (const directory of directories) {
+            const suite = path.basename(directory)
 
-      const [stdout, stderr] = await execute(args)
+            it.concurrent(suite, async function ({expect}) {
+                const featurePaths = (await globby(['*.feature', '*.feature.md'], {cwd: directory})).map(filename => path.join(directory, filename))
+                const args = [...featurePaths]
+                const argumentsPath = path.join(
+                    process.cwd(),
+                    'samples',
+                    suite,
+                    suite + '.arguments.txt'
+                )
+                if (fs.existsSync(argumentsPath)) {
+                    args.push(...fs.readFileSync(argumentsPath, {encoding: 'utf-8'})
+                        .trim()
+                        .split(' '))
+                }
 
-      console.error(stderr)
-      await expect(stdout).toMatchFileSnapshot(
-          path.join(
-              process.cwd(),
-              'samples',
-              suite,
-              suite + '.ndjson'
-          )
-      )
+                const [stdout, stderr] = await execute(args)
+
+                console.error(stderr)
+                await expect(stdout).toMatchFileSnapshot(
+                    path.join(
+                        process.cwd(),
+                        'samples',
+                        suite,
+                        suite + '.ndjson'
+                    )
+                )
+            })
+        }
     })
-  }
+
+    describe('Validation', async () => {
+        const ajv = new Ajv({
+            allErrors: true,
+            loadSchema,
+        })
+        const validate = await ajv.compileAsync<Envelope>(loadSchema('Envelope.json'))
+
+        for (const directory of directories) {
+            const suite = path.basename(directory)
+
+            it.concurrent(suite, async function ({expect}) {
+                let isEmpty = true
+                await pipeline(
+                    [
+                        createReadStream(path.join(
+                        process.cwd(),
+                        'samples',
+                        suite,
+                        suite + '.ndjson')),
+                        new NdjsonToMessageStream(line => JSON.parse(line)),
+                        new Writable({
+                            objectMode: true,
+                            write(envelope, _, callback) {
+                                isEmpty = false
+                                const valid = validate(envelope);
+                                if (valid) {
+                                    callback()
+                                } else {
+                                    callback(new Error('Schema validation errors(s): ' + JSON.stringify(validate.errors)))
+                                }
+                            }
+                        })
+                    ]
+                )
+                if (isEmpty) {
+                    expect.fail('No envelopes in stream')
+                }
+            })
+        }
+    })
 })
 
 async function execute(args: string[]): Promise<[string, string]> {
-  return new Promise((resolve, reject) => {
-    execFile(
-        'npx',
-        [
-          'tsx',
-          './src/cli.ts',
-          ...args,
-        ],
-        {},
-        (error, stdout, stderr) => {
-          if (error) {
-            reject(error)
-          } else {
-            resolve([stdout, stderr])
-          }
-        }
-    )
-  })
+    return new Promise((resolve, reject) => {
+        execFile(
+            'npx',
+            [
+                'tsx',
+                './src/cli.ts',
+                ...args,
+            ],
+            {},
+            (error, stdout, stderr) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve([stdout, stderr])
+                }
+            }
+        )
+    })
+}
+
+function loadSchema(filename: string) {
+    return JSON.parse(fs.readFileSync(`${__dirname}/node_modules/@cucumber/messages/schema/${filename}`, {encoding: 'utf-8'}));
 }
