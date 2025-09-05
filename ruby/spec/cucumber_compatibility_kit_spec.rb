8d02f57f7addd18af488f8b4974ba996d2912032
Luke Hill <lukehill_uk@hotmail.com>
2025-09-05 17:31:08 +0100

    Add in correct spec refs to class methods

--- ruby/spec/cucumber_compatibility_kit_spec.rb
+++ ruby/spec/cucumber_compatibility_kit_spec.rb
@@ -5,7 +5,7 @@ require 'cucumber_compatibility_kit'
 describe CucumberCompatibilityKit do
   let(:features_path) { File.expand_path("#{File.dirname(__FILE__)}/../../features") }
 
-  describe '#feature_code_for' do
+  describe '.feature_code_for' do
     context 'with an example that exists' do
       it 'returns the path of the folder containing the feature code for the example' do
         expect(described_class.feature_code_for('hooks')).to eq("#{features_path}/hooks")
@@ -21,14 +21,14 @@ describe CucumberCompatibilityKit do
     end
   end
 
-  describe '#gherkin' do
+  describe '.gherkin' do
     it 'returns the list of gherkin examples' do
       # Maintaining this list is cumbersome, so we just include a few here
       expect(described_class.gherkin).to include(*%w[attachments empty examples-tables hooks minimal rules])
     end
   end
 
-  describe '#markdown' do
+  describe '.markdown' do
     it 'returns the list of markdown examples' do
       expect(described_class.markdown).to eq(['markdown'])
     end
