f711c118cc3823c44fb3e6b7fd04e1c868f2780a
David Goss <david@davidgoss.co>
2025-09-05 10:54:55 +0100

    prefactor

--- devkit/src/World.ts
+++ devkit/src/World.ts
@@ -1,73 +1,22 @@
-import {
-  AttachmentContentEncoding,
-  Envelope,
-  TimeConversion,
-} from '@cucumber/messages'
-import { Readable } from 'stream'
+import { Envelope } from '@cucumber/messages'
 import { Clock } from './Clock'
+import { ExecutionContext } from './ExecutionContext'
 
-const LOG_MEDIA_TYPE = 'text/x.cucumber.log+plain'
-const LINK_MEDIA_TYPE = 'text/uri-list'
-
-export class World {
+export class World extends ExecutionContext {
   public testStepId: string | undefined
 
   constructor(
-    private readonly clock: Clock,
-    private readonly onMessage: (envelope: Envelope) => void,
+    clock: Clock,
+    onMessage: (envelope: Envelope) => void,
     private readonly testCaseStartedId: string
-  ) {}
+  ) {
+    super(clock, onMessage)
+  }
 
-  async attach(
-    data: Readable | Buffer | string,
-    optionsOrMediaType:
-      | {
-          mediaType: string
-          fileName?: string
-        }
-      | string
-  ): Promise<void> {
-    const options =
-      typeof optionsOrMediaType === 'string'
-        ? { mediaType: optionsOrMediaType }
-        : optionsOrMediaType
-    let body = '',
-      contentEncoding = AttachmentContentEncoding.IDENTITY
-
-    if (typeof data === 'string') {
-      body = data
-    } else if (Buffer.isBuffer(data)) {
-      body = data.toString('base64')
-      contentEncoding = AttachmentContentEncoding.BASE64
-    } else {
-      const chunks = []
-      for await (const chunk of data) {
-        chunks.push(chunk)
-      }
-      body = Buffer.concat(chunks).toString('base64')
-      contentEncoding = AttachmentContentEncoding.BASE64
+  makeAttachmentRelations() {
+    return {
+      testCaseStartedId: this.testCaseStartedId,
+      testStepId: this.testStepId,
     }
-
-    this.onMessage({
-      attachment: {
-        testCaseStartedId: this.testCaseStartedId,
-        testStepId: this.testStepId,
-        body,
-        contentEncoding,
-        mediaType: options.mediaType,
-        fileName: options.fileName,
-        timestamp: TimeConversion.millisecondsSinceEpochToTimestamp(
-          this.clock.now()
-        ),
-      },
-    })
-  }
-
-  async log(text: string): Promise<void> {
-    await this.attach(text, { mediaType: LOG_MEDIA_TYPE })
-  }
-
-  async link(url: string, title?: string): Promise<void> {
-    await this.attach(url, { mediaType: LINK_MEDIA_TYPE, fileName: title })
   }
 }
