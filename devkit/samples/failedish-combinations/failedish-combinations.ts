8760dfbddce08929bc1e3253902e6d020e1f570b
David Goss <david@davidgoss.co>
2026-02-06 21:17:30 +0000

    improve coverage and clarity

--- devkit/samples/failedish-combinations/failedish-combinations.ts
+++ devkit/samples/failedish-combinations/failedish-combinations.ts
@@ -1,19 +1,21 @@
 import { Given } from '@cucumber/fake-cucumber'
 
-Given(/^a step$/, () => {})
-
-Given(/^a failing step$/, () => {
-  throw new Error('whoops')
-})
-
-Given(/^a pending step$/, () => {
-  return 'pending'
+Given(/^a step$/, () => {
+  // no-op
 })
 
 Given(/^a skipped step$/, () => {
   return 'skipped'
 })
 
+Given(/^a pending step$/, () => {
+  return 'pending'
+})
+
 Given(/^an ambiguous (.*?)$/, () => {})
 
 Given(/^(.*?) ambiguous step$/, () => {})
+
+Given(/^a failing step$/, () => {
+  throw new Error('whoops')
+})
